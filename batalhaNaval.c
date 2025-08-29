27739be33c45145187321321771b4d9743fd0ab9
Bruno Jaime <emanuelbrunosj@gmail.com>
2025-08-29 01:01:36 -0300

    desafio batalha naval

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,190 @@
+#include <stdbool.h>
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+#define TAMANHO 10
+#define TAM_PADRAO 3
+#define LARGURA_PADRAO 5
+
+void inicializarTabuleiro(int tabuleiro[TAMANHO][TAMANHO]);
+void exibirTabuleiro(int tabuleiro[TAMANHO][TAMANHO], const char* titulo);
+bool validarEntrada(int valor, int min, int max, const char* mensagem);
+void posicionarNavioHorizontal(int tabuleiro[TAMANHO][TAMANHO], int inicio_i, int inicio_j,
+                               int tamanho);
+void posicionarNavioVertical(int tabuleiro[TAMANHO][TAMANHO], int inicio_i, int inicio_j,
+                             int tamanho);
+void posicionarNavioDiagonal(int tabuleiro[TAMANHO][TAMANHO], int inicio_i, int inicio_j,
+                             int tamanho);
+void aplicarPadrao(int areas_afetadas[TAMANHO][TAMANHO], int centro_i, int centro_j,
+                   int padrao[TAM_PADRAO][LARGURA_PADRAO], const char* nomePadrao);
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    int tabuleiro[TAMANHO][TAMANHO] = {0};
+    int areas_afetadas[TAMANHO][TAMANHO] = {0};
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    int cone[TAM_PADRAO][LARGURA_PADRAO] = {{0, 0, 1, 0, 0}, {0, 1, 1, 1, 0}, {1, 1, 1, 1, 1}};
+    int cruz[TAM_PADRAO][LARGURA_PADRAO] = {{0, 0, 1, 0, 0}, {1, 1, 1, 1, 1}, {0, 0, 1, 0, 0}};
+    int octaedro[TAM_PADRAO][LARGURA_PADRAO] = {{0, 0, 1, 0, 0}, {0, 1, 1, 1, 0}, {0, 0, 1, 0, 0}};
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    int horizontal_inicio_i, horizontal_inicio_j;
+    int vertical_inicio_i, vertical_inicio_j;
+    int diagonal1_inicio_i, diagonal1_inicio_j;
+    int diagonal2_inicio_i, diagonal2_inicio_j;
+    int centro_i, centro_j;
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    printf("=== BATALHA NAVAL - TODOS OS NÍVEIS ===\n\n");
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    printf("=== NÍVEL NOVATO ===\n");
+    printf("Posicionamento de Navios Básicos\n\n");
+
+    printf("Navio Horizontal (3 células):\n");
+    do {
+        printf("Linha inicial (0-9): ");
+        scanf("%d", &horizontal_inicio_i);
+    } while (!validarEntrada(horizontal_inicio_i, 0, 9, "Linha deve estar entre 0 e 9"));
+
+    do {
+        printf("Coluna inicial (0-7): ");
+        scanf("%d", &horizontal_inicio_j);
+    } while (!validarEntrada(horizontal_inicio_j, 0, 7, "Coluna deve estar entre 0 e 7"));
+
+    printf("\nNavio Vertical (3 células):\n");
+    do {
+        printf("Linha inicial (0-7): ");
+        scanf("%d", &vertical_inicio_i);
+    } while (!validarEntrada(vertical_inicio_i, 0, 7, "Linha deve estar entre 0 e 7"));
+
+    do {
+        printf("Coluna inicial (0-9): ");
+        scanf("%d", &vertical_inicio_j);
+    } while (!validarEntrada(vertical_inicio_j, 0, 9, "Coluna deve estar entre 0 e 9"));
+
+    printf("\n=== NÍVEL AVENTUREIRO ===\n");
+    printf("Posicionamento de Navios Adicionais\n\n");
+
+    printf("Navio Diagonal 1 (4 células):\n");
+    do {
+        printf("Linha inicial (0-6): ");
+        scanf("%d", &diagonal1_inicio_i);
+    } while (!validarEntrada(diagonal1_inicio_i, 0, 6, "Linha deve estar entre 0 e 6"));
+
+    do {
+        printf("Coluna inicial (0-6): ");
+        scanf("%d", &diagonal1_inicio_j);
+    } while (!validarEntrada(diagonal1_inicio_j, 0, 6, "Coluna deve estar entre 0 e 6"));
+
+    printf("\nNavio Diagonal 2 (4 células):\n");
+    do {
+        printf("Linha inicial (0-6): ");
+        scanf("%d", &diagonal2_inicio_i);
+    } while (!validarEntrada(diagonal2_inicio_i, 0, 6, "Linha deve estar entre 0 e 6"));
+
+    do {
+        printf("Coluna inicial (0-6): ");
+        scanf("%d", &diagonal2_inicio_j);
+    } while (!validarEntrada(diagonal2_inicio_j, 0, 6, "Coluna deve estar entre 0 e 6"));
+
+    printf("\n=== NÍVEL MESTRE ===\n");
+    printf("Centro das Habilidades Especiais\n\n");
+
+    printf("Linha do centro (1-8): ");
+    scanf("%d", &centro_i);
+    while (!validarEntrada(centro_i, 1, 8, "Linha deve estar entre 1 e 8")) {
+        printf("Linha do centro (1-8): ");
+        scanf("%d", &centro_i);
+    }
+
+    printf("Coluna do centro (2-7): ");
+    scanf("%d", &centro_j);
+    while (!validarEntrada(centro_j, 2, 7, "Coluna deve estar entre 2 e 7")) {
+        printf("Coluna do centro (2-7): ");
+        scanf("%d", &centro_j);
+    }
+
+    posicionarNavioHorizontal(tabuleiro, horizontal_inicio_i, horizontal_inicio_j, 3);
+    posicionarNavioVertical(tabuleiro, vertical_inicio_i, vertical_inicio_j, 3);
+
+    posicionarNavioDiagonal(tabuleiro, diagonal1_inicio_i, diagonal1_inicio_j, 4);
+    posicionarNavioDiagonal(tabuleiro, diagonal2_inicio_i, diagonal2_inicio_j, 4);
+
+    exibirTabuleiro(tabuleiro, "TABULEIRO COMPLETO COM NAVIOS (0-ÁGUA, 1-NAVIO)");
+
+    aplicarPadrao(areas_afetadas, centro_i, centro_j, cone, "CONE");
+    exibirTabuleiro(areas_afetadas, "ÁREAS AFETADAS PELO CONE (0-NÃO, 1-SIM)");
+
+    aplicarPadrao(areas_afetadas, centro_i, centro_j, cruz, "CRUZ");
+    exibirTabuleiro(areas_afetadas, "ÁREAS AFETADAS PELA CRUZ (0-NÃO, 1-SIM)");
+
+    aplicarPadrao(areas_afetadas, centro_i, centro_j, octaedro, "OCTAEDRO");
+    exibirTabuleiro(areas_afetadas, "ÁREAS AFETADAS PELO OCTAEDRO (0-NÃO, 1-SIM)");
 
     return 0;
+}
+
+bool validarEntrada(int valor, int min, int max, const char* mensagem) {
+    if (valor < min || valor > max) {
+        printf("Entrada inválida! %s\n", mensagem);
+        return false;
+    }
+    return true;
+}
+
+void posicionarNavioHorizontal(int tabuleiro[TAMANHO][TAMANHO], int inicio_i, int inicio_j,
+                               int tamanho) {
+    for (int j = inicio_j; j < inicio_j + tamanho; j++) {
+        if (j < TAMANHO) {
+            tabuleiro[inicio_i][j] = 1;
+        }
+    }
+}
+
+void posicionarNavioVertical(int tabuleiro[TAMANHO][TAMANHO], int inicio_i, int inicio_j,
+                             int tamanho) {
+    for (int i = inicio_i; i < inicio_i + tamanho; i++) {
+        if (i < TAMANHO) {
+            tabuleiro[i][inicio_j] = 1;
+        }
+    }
+}
+
+void posicionarNavioDiagonal(int tabuleiro[TAMANHO][TAMANHO], int inicio_i, int inicio_j,
+                             int tamanho) {
+    for (int d = 0; d < tamanho; d++) {
+        int i = inicio_i + d;
+        int j = inicio_j + d;
+        if (i < TAMANHO && j < TAMANHO) {
+            tabuleiro[i][j] = 1;
+        }
+    }
+}
+
+void aplicarPadrao(int areas_afetadas[TAMANHO][TAMANHO], int centro_i, int centro_j,
+                   int padrao[TAM_PADRAO][LARGURA_PADRAO], const char* nomePadrao) {
+    for (int i = 0; i < TAMANHO; i++) {
+        for (int j = 0; j < TAMANHO; j++) {
+            areas_afetadas[i][j] = 0;
+        }
+    }
+
+    for (int i = 0; i < TAM_PADRAO; i++) {
+        for (int j = 0; j < LARGURA_PADRAO; j++) {
+            int x = centro_i - 1 + i;
+            int y = centro_j - 2 + j;
+            if (x >= 0 && x < TAMANHO && y >= 0 && y < TAMANHO) {
+                areas_afetadas[x][y] = padrao[i][j];
+            }
+        }
+    }
+}
+
+void exibirTabuleiro(int tabuleiro[TAMANHO][TAMANHO], const char* titulo) {
+    printf("\n=== %s ===\n", titulo);
+    printf("   0 1 2 3 4 5 6 7 8 9\n");
+    for (int i = 0; i < TAMANHO; i++) {
+        printf("%d  ", i);
+        for (int j = 0; j < TAMANHO; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
 }
